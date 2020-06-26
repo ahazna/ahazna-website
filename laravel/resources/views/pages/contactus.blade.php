f77dc073f77eb2b988f7fd237560b37352d24e3a
ahazna <contact@ahazna.com>
2020-06-26 19:50:31 +0300

    contacts pge is modified

--- laravel/resources/views/pages/contactus.blade.php
+++ laravel/resources/views/pages/contactus.blade.php
@@ -3,15 +3,22 @@
 @section('content')
 
 
-<div class="container-fluid m-auto background-img d-flex w-100 p-3 mx-auto flex-column">
+<div class="container-fluid m-auto h-100 background-img d-flex w-100 p-3 mx-auto flex-column">
 
     <div class="inner container m-auto p-3 mx-auto">
         
        <main role="main" class="inner cover">
-        <h1 class="cover-heading">Cover your page.</h1>
-        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-        <p class="lead">
-          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+        <h1 class="cover-heading">CONTACT US</h1>
+
+    <div class="contact-us-bg">
+
+        <p class="lead" >Adress : 16 kebelle, above catholic church</p>
+        <p class="lead" >Mekelle, Tigray, Ethiopia </p>
+
+        <p class="lead" style="border-bottom: 1px solid white;">Phone : (+251) 914 024366</p>
+    </div>
+        <p class="lead" style="text-align: center" >
+          <a href="mailto:contactus@ahazna.com" class="btn btn-lg btn-secondary">Contact@ahazna.com</a>
         </p>
       </main>
 
