204d31328e889a9cd64c531133bc3d59f7a1026e
ahazna <contact@ahazna.com>
2020-06-27 16:36:13 +0300

    responsive design of team is fixed

--- laravel/resources/views/pages/team.blade.php
+++ laravel/resources/views/pages/team.blade.php
@@ -2,9 +2,8 @@
 
 @section('content')
 
-<div class="container-fluid m-auto h-100 d-flex w-100 p-3 mx-auto flex-column">
 
-	<div class="container m-auto p-3 mx-auto">
+	<div class="container m-auto p-3 mx-auto d-none d-md-block">
 
        
 	   <div class="row text-center">
@@ -74,6 +73,78 @@
 
 	</div>
 
-</div>
 
-@endsection
+<div class="container-fluid m-auto d-flex w-100 p-3 mx-auto flex-column d-sm-none">
+
+
+<div class="container m-auto p-3 mx-auto d-sm-none">
+
+       
+	   <div class="row text-center">
+
+	        <!--Grid column-->
+	        <div class="col-lg-4 col-md-6 mb-4 " style="border-bottom: 1px solid #a3685d;">
+
+	  		 <h4 class="cover-heading">CO-FOUNDER & CEO</h4>
+
+	            <div class="avatar mx-auto mb-3 p-3">
+	                <a href="{{ asset('storage/images/efrem-pro.jpg')}}"><img src="{{ asset('storage/images/efrem-pro.jpg')}}" class="rounded-circle z-depth-1" width=200 height=200></a>
+	            </div>
+
+	            <h6 class="font-weight-bold mb-3">ENG. EFREM GEBREMEDHIN</h6>
+	            <h6>Full Stack Developer</h6>
+
+	        </div>
+
+	   </div>
+	        <!--Grid column-->
+
+      		 <h4 class="cover-heading">CO-FOUNDERS</h4>
+
+	<div class="row text-center">
+
+            <!--Grid column-->
+            <div class="col-lg-4 col-md-6 mb-4">
+
+
+                <div class="avatar mx-auto mb-3 p-3">
+                    <img src="https://mdbootstrap.com/img/Photos/Avatars/sebastian%20(1).jpg" class="rounded-circle z-depth-1">
+                </div>
+
+                <h6 class="font-weight-bold mb-3">DR. MEKONNEN GEBREHIWOT</h6>
+                <h6>Electromechanics</h6>
+
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-lg-4 col-md-6 mb-4">
+
+                <div class="avatar mx-auto mb-3 p-3">
+                    <img src="https://mdbootstrap.com/img/Photos/Avatars/sebastian%20(1).jpg" class="rounded-circle z-depth-1">
+                </div>
+
+                <h6 class="font-weight-bold mb-3">DR. ATAKLTI GEBREMEDHIN</h6>
+                <h6>Electromechanics</h6>
+
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-lg-4 col-md-6 mb-4">
+                
+                <div class="avatar mx-auto mb-3 p-3">
+                    <img src="https://mdbootstrap.com/img/Photos/Avatars/sebastian%20(1).jpg" class="rounded-circle z-depth-1">
+                </div>
+
+                <h6 class="font-weight-bold mb-3">ATO. YOSIEF GIRMAY</h6>
+                <h6>Digital Marketing</h6>
+
+            </div>
+            <!--Grid column-->
+
+    </div>
+
+	</div>
+</div>
+ @endsection
