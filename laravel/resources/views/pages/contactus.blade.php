23e732f20da0a4745be6eaac4939765301617c61
ahazna <contact@ahazna.com>
2020-06-30 01:58:02 +0300

    projects is responsive and contacts page is cleared

--- laravel/resources/views/pages/contactus.blade.php
+++ laravel/resources/views/pages/contactus.blade.php
@@ -7,75 +7,27 @@
 
     <div class="inner container m-auto p-3 mx-auto">
         
-       <main role="main" class="inner cover">
-        <h1 class="cover-heading">CONTACT US</h1>
+        <main role="main" class="inner cover">
+            <h1 class="cover-heading">CONTACT US</h1>
 
-    <div class="contact-us-bg">
+            <div class="contact-us-bg">
 
-        <p class="lead" >Adress : 16 kebelle, above catholic church</p>
-        <p class="lead" >Mekelle, Tigray, Ethiopia </p>
+                <p class="lead" >Adress : 16 kebelle, above catholic church</p>
+                <p class="lead" >Mekelle, Tigray, Ethiopia </p>
 
-        <p class="lead" style="border-bottom: 1px solid white;">Phone : (+251) 914 024366</p>
-    </div>
-        <p class="lead" style="text-align: center" >
-          <a href="mailto:contactus@ahazna.com" class="btn btn-lg btn-secondary">Contact@ahazna.com</a>
-        </p>
-      </main>
-
-    </div>
-
-<!-- <div class="inner container m-auto cover p-3 mx-auto">
-    <div class="row col-md-6 m-auto card text-center">
-      <div class="card-header">
-        CONTACT US
-      </div>
-
-        <div class="card-body">
-            <h5 class="card-title">Ahaz Electronic & Software Solutions</h5>
-            <p class="card-text">MEKELLE, TIGRAY, ETHIOPIA</p>
-            <p class="card-text">PHONE: +251 914 0243 66</p>
-            <a href="mailto:contact@nahazna.com" class="btn btn-primary">Contact@ahazna.com</a>
-        </div>
-
-      <div class="card-footer text-muted">
-            Thank you for reaching us
-        </div>
-    </div> -->
-</div>
-
-<!--   <div class="inner cover p-3 mx-auto">
-    <div class="card col-md-6 offset-3 p-0">
-            <h5 class="card-header">Message Us</h5>
-            <div class="card-body">
-                 <form>
-
-                    <div class="form-group">
-                        <label for="inputAddress">Name</label>
-                        <input type="text" class="form-control" id="inputName" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="inputAddress">Email</label>
-                        <input type="Email" class="form-control" id="inputEmail" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="inputphone">Phone</label>
-                        <input type="tel" class="form-control" id="phone" maxlength="14" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea3">Text</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea3" rows="4" placeholder="Send us your message" required></textarea>
-                    </div>
-
-                    <button type="submit" class="btn btn-primary btn-lg btn-block">Send</button>
-                </form> 
+                <p class="lead" style="border-bottom: 1px solid white;">Phone : (+251) 914 024366</p>
             </div>
-        </div>
-  </div> -->
+
+            <p class="lead" style="text-align: center" >
+              <a href="mailto:contactus@ahazna.com" class="btn btn-lg btn-secondary">Contact@ahazna.com</a>
+            </p>
+            
+        </main>
+
+    </div>
 
 </div>
-        
+
+     
 
 @endsection
--- laravel/resources/views/pages/projects.blade.php
+++ laravel/resources/views/pages/projects.blade.php
@@ -2,31 +2,55 @@
 
 @section('content')
 
-<div class="container-fluid m-auto d-flex w-100 h-100 image-projects p-3 mx-auto flex-column">
-	
-	<div class="inner container m-auto p-2 mx-auto" style="color: black; font-family: 'Abril Fatface', cursive; background-color: #ffffff8c">
+
+   <div class="container-fluid m-auto h-100 image-projects d-flex w-100 p-3 mx-auto flex-column">
+
+    <div class="inner container m-auto p-3 mx-auto d-none d-sm-block" style="color: black; font-family: 'Abril Fatface', cursive; background-color: #ffffff8c;">
 		
 		<h1>Do you have a project idea reach us!</h1>
 
 		<div class="row d-flex justify-content-center p-3">
 
-			    	
 	    	<span class="input-group-text"><i class="fa fa-phone fa-2x" aria-hidden="true"></i></span>
 		  	<p class="lead m-0">
       			<a class="btn btn-lg btn-secondary mr-2">(+251) 914 024 366</a>
     		</p>
 
-		
 		    <span class="input-group-text"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></span>
 		  	<p class="lead m-0">
       		<a href="mailto:contactus@ahazna.com" class="btn btn-lg btn-secondary">Contact@ahazna.com</a>
     		</p>
 
+	    </div> 
+	</div>
+
+	<div class="inner container m-auto p-2 mx-auto d-sm-none" style="color: black; font-family: 'Abril Fatface', cursive; background-color: #ffffff8c">
+		
+		<h4>Do you have a project idea reach us!</h4>
+
+		<div class="row d-flex justify-content-center p-3">
+
+		<div class="col-lg-4 col-md-6 mb-4 input-group justify-content-center">
+	    	<span class="input-group-text"><i class="fa fa-phone" aria-hidden="true"></i></span>
+		  	<p class="lead m-0">
+      			<a class="btn btn-secondary mr-2">(+251) 914 024 366</a>
+    		</p>
+		</div>
+
+		<div class="col-lg-4 col-md-6 mb-4 input-group justify-content-center">
+
+		    <span class="input-group-text"><i class="fa fa-envelope " aria-hidden="true"></i></span>
+		  	<p class="lead m-0">
+      		<a href="mailto:contactus@ahazna.com" class="btn btn-secondary">Contact@ahazna.com</a>
+    		</p>
+		</div>
 
 	    </div>
 
 	</div>
 
 </div>
+
+
 
 @endsection
