d1772c2173b5687705e8e10e045b2c5a68973881
ahazna <contact@ahazna.com>
2020-09-22 17:08:30 +0300

    contact us page design has been made

--- laravel/resources/views/pages/contactus.blade.php
+++ laravel/resources/views/pages/contactus.blade.php
@@ -4,21 +4,61 @@
 
 <div class="d-none d-sm-block h-100">
 
-<div class="container-fluid m-auto h-100 background-img d-flex w-100 p-3 mx-auto flex-column">
+<div class="container-fluid m-auto h-100 d-flex w-100 p-3 mx-auto flex-column">
 
     <div class="inner container m-auto p-3 mx-auto">
         
-        <main role="main" class="inner p-3" style="color: black; background-color: #c870614d;">
-            <h1 class="cover-heading">CONTACT US</h1>
+        <main role="main" class="inner p-3">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="mb-3">
+                        <i class="fa fa-map-marker fa-5x" aria-hidden="true"></i> <span>MidereGenet Taxi, kebele 19, Mekelle, Tigray, Ethiopia</span>
+                    </div>
 
-            <div class="contact-us-bg p-2">
+                    <div class="mb-3">
+                        <i class="fa fa-envelope fa-1x" aria-hidden="true"></i> <a href="mailto:contact@ahazna.com">Contact@ahazna.com</a>               
+                     </div>
 
-                <p class="lead" >Adress : on the right side of Genfel Hotel, Kebele 19</p>
-                <p class="lead" >Mekelle, Tigray, Ethiopia </p>
+                    <div>
+                        <i class="fa fa-phone-square fa-1x" aria-hidden="true"></i> (+251)-914-024366
+                    </div>
+                    
+                </div>
 
+                <div class="col-md-6">
+                    <h1 class="cover-heading mb-3">Get in Touch</h1>
+                    <p class="mb-3" style="color: white;">Feel free to get in touch with us. We are always open to discussing new projects and feel free to ask us questions regarding our company. We will get back to you soon.</p>
+
+
+                    <form style="text-align: left;">
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                              <label for="name">Name</label>
+                              <input type="text" class="form-control" id="inputName">
+                            </div>
+                            <div class="form-group col-md-6">
+                              <label for="email">Email</label>
+                              <input type="email" class="form-control" id="inputEmail">
+                            </div>
+                          </div>
+
+                          <div class="form-group">
+                            <label for="message">Message</label>
+                            <textarea class="form-control" id="message" rows="3"></textarea>
+                          </div>
+
+                        <button type="submit" class="btn btn-primary btn-block mb-2">Submit</button>
+
+                    </form>
+
+                </div>
+
+                    
             </div>
 
-            <div class="mt-2" style="display: inline-flex;">
+           
+
+           <!--  <div class="mt-2" style="display: inline-flex;">
                 <p class="lead mr-2" style="text-align: center">
                   <a class="btn btn-lg btn-secondary">(+251) 914 024 366</a>
                 </p>
@@ -26,7 +66,7 @@
                 <p class="lead" style="text-align: center">
                   <a href="mailto:contactus@ahazna.com" class="btn btn-lg btn-secondary">Contact@ahazna.com</a>
                 </p>
-            </div>
+            </div>  -->
 
         </main>
 
