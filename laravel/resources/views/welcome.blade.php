683d8ce2d1763b3ff2ce8281cc0bbd36ea98e4ba
ahazna <contact@ahazna.com>
2020-09-26 21:25:43 +0300

    welcome page is modified

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -30,8 +30,23 @@
     <div class="container-fluid h-100 m-auto d-flex w-100 h-100 p-3 mx-auto flex-column">
         
         <div class="inner container m-auto p-3 mx-auto">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-5 col-lg-6 order-md-2">
+                    <img src="{{ asset('storage/images/illustration.png')}}" class="img-fluid mw-md-150 mw-lg-130 mb-6 mb-md-0 aos-init aos-animate" alt="..." data-aos="fade-up" data-aos-delay="100">
+                </div>
 
-           <!--  <div class="divider" style="font-family:'Lato';"><h2><span></span><span>WELCOME TO AHAZ</span><span></span></h2></div> -->
+                <div class="col-md-6">
+                    <div class="display-4 text-center text-md-left">
+                      Welcome to <span style="font-family: 'Abril Fatface', cursive; color: #dc8577;">AHAZ</span> <br>
+                      Digital Solutions
+                    </div>
+                    <p class="lead text-center text-md-left mb-6 mb-lg-8" style="color: #a5b9cc;">
+              Build a beautiful, modern softwares, websites and android applications built with user centered design methodology.
+            </p>
+                </div>
+                
+            </div>
+           
             
         </div>
 
